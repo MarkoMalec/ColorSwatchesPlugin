91edf9fa9e9b2ec0b36af7bea81731ae513a7835
Marko Malec <cropsychooo@yahoo.com>
2023-04-06 10:07:57 +0200

    Specified the product thumbnail element selector so it wouldn't grab any other existing images inside the product loop item.

--- assets/js/events/image-change.js
+++ assets/js/events/image-change.js
@@ -1,10 +1,9 @@
 jQuery(document).ready(function ($) {
-    console.log('click');
     $('.swatch_wrap').on('click', function () {
       var color_data = $(this).find('.color_swatch').data('color');
       var image_url = $(this).find('.color_swatch').data('image-url');
-      $(this).parents('.splide__slide.product').find('img').attr('src', image_url);
-      $(this).parents('.splide__slide.product').find('img').attr('srcset', image_url);
+      $(this).parents('.splide__slide.product').find('img.attachment-woocommerce_thumbnail').attr('src', image_url);
+      $(this).parents('.splide__slide.product').find('img.attachment-woocommerce_thumbnail').attr('srcset', image_url);
       $(this).parents('.splide__slide.product').find('source').attr('srcset', image_url);
       $(this).addClass('active').siblings().removeClass('active');
     });
